3d83f83e3248d2a072490623a7d10a31ad70c6d4
csandoval18 <christian_alvites@outlook.com>
2022-08-21 00:05:34 -0500

    created deployment bash file

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,3 +1,8 @@
-#!/bin/bash
+#!/bin/fish
 
-echo hello
+echo "What should the version be?"
+read VERSION
+
+docker build -t csandoval18/forum-app:$VERSION .
+docker push csandoval18/forum-app:$VERSION 
+ssh root@164.90.252.96 "docker pull csandoval18/forum-app:$VERSION && docker tag csandoval18/forum-app:$VERSION api:$VERSION && dokku deploy api $VERSION"
